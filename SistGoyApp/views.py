ab8084d79f23be79b0f6850f2d44c4297d691b33
SistGoyPeru <alexgoyzueta2018@gmail.com>
2021-05-05 21:28:16 -0500

    video 28 ok

--- SistGoyApp/views.py
+++ SistGoyApp/views.py
@@ -4,15 +4,15 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Home")
+    return render(request,"SistGoyApp/home.html")
 
 def servicios(request):
-    return HttpResponse("Servicios")
+    return render(request,"SistGoyApp/servicios.html")
 
 def blog(request):
-    return HttpResponse("Blog") 
+    return render(request,"SistGoyApp/blog.html")
 
 def contacto(request):
-    return HttpResponse("Contacto")     
+    return render(request,"SistGoyApp/contacto.html")   
 
 
